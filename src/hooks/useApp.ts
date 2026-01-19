64b357cd13b8e3c3cb8afc6d3fc4de3f312f1e35
timbel-gio-desktop <mpc.jnv.mapa@gmail.com>
2026-01-19 17:39:16 -0300

    feat: refactor Home styles to index.css and fix global useApp hook

---
+++ src/hooks/useApp.ts
@@ -0,0 +1,23 @@
+// Aqui centralizamos os imports que você usará em quase todas as telas
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
+import ReactDOM from 'react-dom/client'
+import '../index.css' // <--- ESTA LINHA É OBRIGATÓRIA!
+
+
+
+export function useApp() {
+  // Inicializamos as ferramentas
+  const navigate = useNavigate();
+  const params = useParams();
+  const location = useLocation();
+
+  // Retornamos tudo em um único objeto mestre
+  return {
+    navigate,
+    params,
+    location,
+    useEffect,
+    useState
+  };
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+//IMPORTS DESABILITADOS PARA TESTAR O IMPORT GLOBAL
+//import React from 'react'
+//import ReactDOM from 'react-dom/client'
+//import App from './App.tsx'
+//import { useNavigate } from 'react-router-dom';
+//import '../index.css' //
+
+
+//IMPORT GLOBAL
+import { useApp } from '../hooks/useApp';
+
 
 export default function Home() {
-  const navigate = useNavigate();
+  const { navigate } = useApp();
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-6 text-center bg-slate-900 text-white">
       <h1 className="text-4xl font-bold text-emerald-400 mb-4">Emagrac.ia</h1>
